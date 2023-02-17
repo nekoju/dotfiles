8627976fdb6deb32c03df68c1d91d15d665bb58f
Neko Juers <njuers@Nekos-MacBook-Pro.local>
2023-02-16 19:10:54 -0500

    Fish working now with stripped down config.
    Move dotfiles to homedir. Untested.
    
    neovim working.
    
    Dracula colorscheme working.
    
    Dracula working in fish.
    
    Remove tmux plugins from tracking.
    
    Dracula working in Vim.
    
    Colorschemes working across vim and iterm.
    
    Add Karabiner for mouse mapping.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,34 +1,19 @@
-# set normal (set_color normal)
-# set magenta (set_color magenta)
-# set yellow (set_color yellow)
-# set green (set_color green)
-# set red (set_color red)
-# set gray (set_color -o black)
 set -Ux VISUAL nvim
 set -Ux EDITOR $VISUAL
+fish_vi_key_bindings
 # source (dirname (status -f))/init.fish
 
 # if tmux is executable and not inside a tmux session, then try to attach.
 # if attachment fails, start a new session
 
 # set -x TERMINFO $TERMINFO $HOME/.local/share/terminfo/74/
-set -x TERM tmux-256color
-if status is-interactive
-and not set -q TMUX
-    exec tmux
-end
+# set -x TERM tmux-256color
+# if status is-interactive
+# and not set -q TMUX
+#     exec tmux
+# end
 
 set -x PYTHONBREAKPOINT ipdb.set_trace
-
- # Fish git prompt
-set -x __fish_git_prompt_showdirtystate 'yes'
-set -x __fish_git_prompt_showstashstate 'yes'
-set -x __fish_git_prompt_showuntrackedfiles 'yes'
-set -x __fish_git_prompt_showupstream 'yes'
-set -x __fish_git_prompt_color_branch yellow
-set -x __fish_git_prompt_color_upstream_ahead green
-set -x __fish_git_prompt_color_upstream_behind red
-
 
 # Proper terminal behavior with unicode
 set -x LC_ALL en_US.UTF-8
@@ -40,7 +25,7 @@ bind -M default v edit_cmd
 # for nvim
 
 # Use ipdb for debugging
-# set -x PYTHONBREAKPOINT ipdb.set_trace
+set -x PYTHONBREAKPOINT ipdb.set_trace
 
 set -x JAVA_HOME /usr/local/opt/openjdk/bin
 
@@ -53,23 +38,21 @@ set -gx LDFLAGS $LDFLAGS "-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/ll
 set -gx CPPFLAGS $CPPFLAGS "-I/usr/local/opt/llvm/include -I/usr/local/opt/llvm/include/c++/v1/"
 
 set -x ENABLE_USER_SITE "False"
-
-set -x PATH $JAVA_HOME $PATH
+set -x PATH /opt/homebrew/bin $PATH
 set -x PATH /opt/local/bin /opt/local/sbin $PATH
 set -x MANPATH /opt/local/share/man (manpath)
-set -x PATH /usr/local/opt/llvm/bin $PATH
 set -x PATH $HOME/bin $PATH
-set -x PATH $HOME/bin/edirect $PATH
 set -x PATH $HOME/.cargo/bin $PATH
 set -x PATH $HOME/.gitaliases $PATH
-set -x PATH $HOME/edirect $PATH
+set -x PATH /opt/homebrew/ $PATH
 
-set -gx LDFLAGS "-L/usr/local/opt/bzip2/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/bzip2/include"
-set -gx LDFLAGS $LDFLAGS "-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib"
-set -gx CPPFLAGS $CPPFLAGS "-I/usr/local/opt/llvm/include -I/usr/local/opt/llvm/include/c++/v1/ -I$JAVA_HOME/include -I$JAVA_HOME/include/darwin"
+# set -gx LDFLAGS "-L/usr/local/opt/bzip2/lib"
+# set -gx CPPFLAGS "-I/usr/local/opt/bzip2/include"
+# set -gx LDFLAGS $LDFLAGS "-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib"
+# set -gx CPPFLAGS $CPPFLAGS "-I/usr/local/opt/llvm/include -I/usr/local/opt/llvm/include/c++/v1/ -I$JAVA_HOME/include -I$JAVA_HOME/include/darwin"
 
 
 fish_ssh_agent
 
 thefuck --alias | source
+oh-my-posh init fish --config ~/.config/fish/dracula.omp.json | source
