0d64cdfbb5b355fce00cf8018cc9127cf0d0a4b5
Neko Juers <neko.juers@gmail.com>
2023-03-16 14:19:43 +0000

    Add .bash_profile to setup.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 git clone --single-branch --branch servers https://github.com/nekoju/dotfiles.git ~/dotfiles \
 	|| cd ~/dotfiles && git pull --rebase
 
-files=( .bashrc .aliases .gitconfig .gitaliases )
+files=( .bashrc .aliases .gitconfig .gitaliases .bash_profile )
 for file in "${files[@]}"; do
     echo linking "$file"
     mv ~/"$file" ~/"$file".bak \
